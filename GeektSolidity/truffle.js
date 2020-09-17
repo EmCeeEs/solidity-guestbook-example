243ff3221433ab5352aaedcfc024e69d79665b29
EmCeeEs <ab22uhrnachtunruhe@gmail.com>
2020-09-17 14:24:14 +0200

    Compile.

--- GeektSolidity/truffle.js
+++ GeektSolidity/truffle.js
@@ -2,8 +2,13 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 8545,
+      port: 9545,
       network_id: "*" // set to low number so deployment works?
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.4.9",
     }
   }
 };
